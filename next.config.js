0f1229f16fd8fed49e7a981c6fda1fa3bfd556d3
5no0p <mmoaaa1989@hotmail.com>
2021-05-05 13:52:48 +0200

    package.lock

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     basePath: '/next-blog',
-    assetPrefix: '/next-blog',
+    assetPrefix: 'https://cdn.statically.io/gh/5no0p/5no0p.github.io/gh-pages/',
     distDir: 'build',
 }
